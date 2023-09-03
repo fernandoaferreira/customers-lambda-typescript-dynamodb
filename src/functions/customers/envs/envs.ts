c41bb23b6400323c4ac1916e96fdc5e286b366fb
fernandoaferreira <fernandoantunes.ferreira@hotmail.com>
2023-09-03 19:47:44 -0300

    update envs

--- src/functions/customers/envs/envs.ts
+++ src/functions/customers/envs/envs.ts
@@ -2,9 +2,7 @@ const ENVS_VARS = {
     SECRET_JWT: process.env.SECRET_JWT,
     USER_ADMIN: process.env.USER_ADMIN,
     PASSWORD_ADMIN: process.env.PASSWORD_ADMIN,
-    TABLE_NAME: process.env.TABLE_NAME,
-    LIMIT_TIME_CACHE: process.env.LIMIT_TIME_CACHE
-    LIMIT_TIME_CACHE: process.env.TESTE
+    TABLE_NAME: process.env.TABLE_NAME
 }
 
 export default ENVS_VARS;
